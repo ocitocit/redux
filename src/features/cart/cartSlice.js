8942ce849468723d0b82fb162f98a6cc67f8cf2d
ocitocit <ocritocrit@gmail.com>
2023-03-23 22:44:59 +0700

    base reducer on componentc

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,17 +1,25 @@
+// Importing createSlice from "@reduxjs/toolkit" library
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
+// Declaring initialState object with some properties
 const initialState = {
-  cartItems: [],
-  amount: 0,
+  cartItems: cartItems,
+  amount: 5,
   total: 0,
   isLoading: true,
 };
 
+// Creating a new slice of state for cart items using createSlice function
+// from redux toolkit by passing name of the slice and initial state object.
 const cartSlice = createSlice({
   name: "cart",
   initialState,
 });
 
-// console.log(cartSlice);
-
+// Exporting the reducer part of the slice so that it can be included in
+// the store in combination with other reducers.
 export default cartSlice.reducer;
+
+// console.log(cartSlice) -> This line is commented out, it logs the created slice
+// object to the console.
