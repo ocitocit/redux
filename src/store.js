f8b52e492e680e217d9139e3a900579754669709
ocitocit <ocritocrit@gmail.com>
2023-03-25 00:22:48 +0700

    add modal reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 // Importing the configureStore function and the cartReducer from respective modules in our project.
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
+import modalReducer from "./features/modal/modalSlice";
 
 // Creating a Redux store with a 'cart' reducer, which would contain the state for the cart feature of our app.
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
