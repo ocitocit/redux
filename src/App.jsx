6044791e2bcfd7c948fbef63416c989cdb415386
ocitocit <ocritocrit@gmail.com>
2023-03-25 13:20:46 +0700

    modal working

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,7 +16,7 @@ function App() {
 
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+import { closeModal } from "../features/modal/modalSlice";
 
 const Modal = () => {
+  const dispatch = useDispatch();
+
   return (
     <aside className="modal-container">
       <div className="modal">
         <h4>remove all items from your shopping cart?</h4>
         <div className="btn-container">
-          <button className="btn confirm-btn" type="button">
+          <button
+            className="btn confirm-btn"
+            type="button"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
             confirm
           </button>
-          <button className="btn clear-btn" type="button">
+          <button
+            className="btn clear-btn"
+            type="button"
+            onClick={() => {
+              dispatch(closeModal());
+            }}
+          >
             cancel
           </button>
         </div>
